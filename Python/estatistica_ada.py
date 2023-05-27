f44edd68f843096633e34c0c425e5073eef14993
budkee <camila.budke@gmail.com>
2023-05-27 16:11:41 -0400

    Create estatistica_ada.py

---
+++ Python/estatistica_ada.py
@@ -0,0 +1,15 @@
+# Correlação de Pearson
+# Número da amostra n alunos
+n = 10
+# Notas por disciplina
+matematica = [5, 8, 7, 10, 6, 7, 9, 3, 8, 2]
+estatistica = [6, 9, 8, 10, 5, 7, 8, 4, 6, 2]
+# Somatório das notas
+somaMat = sum(matematica)
+somaEst = sum(estatistica)
+# Somatório dos produtos entre as notas
+# Calcular os produtos
+produtos = []
+for notaM in matematica:
+    
+print(produtos)
