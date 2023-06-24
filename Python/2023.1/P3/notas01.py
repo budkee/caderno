52370c5850c2afef36040f258b5e24e14585d6aa
budkee <camila.budke@gmail.com>
2023-06-24 12:30:18 -0400

    (24/06) Dicionários: CRUD e acesso aos elementos

--- Python/2023.1/P3/notas01.py
+++ Python/2023.1/P3/notas01.py
@@ -1,57 +1,94 @@
-# Programa: nomes00.py
-# Programador:
-# Data:
+# Programa notas00.py
+# Programador: budkee
+# Data: 23/06/2023
 
-# Este programa lê um registro com o número, nome, as 3 notas das
-# provas e as 2 notas de trabalho da disciplina de AlgProg I. A
-# média das provas é dada por MP = (P1+P2+P3)/3.0, a média dos
-# trabalhos por MT = (T1 + T2)/2.0 e a média final por
-# MF = 0.75*MP + 0.25*MT. O programa computa a média final e
-# imprime o número, nome e a média final de cada aluno.
-
+# A avaliação da disciplina de Algoritmos e Programação I
+# é composta de três provas e dois trabalhos. Projete um
+# programa que leia um conjunto de alunos com as respectivas notas
+# das provas e dos trabalhos. O programa deve ordenar os alunos pelo
+# primeiro nome usando o método de ordenação por inserção
+# (insertionSort). O programa deve imprimir a lista dos alunos ordenada
+# pelo primeiro nome.
 # início do módulo principal
 
-# descrição das variáveis e estruturas utilizadas
-# list chaves[], dados[]
-# dict estudante{}
-# float mediaprovas, mediatrabalhos, mediafinal
-# int tam
+# Variáveis
+# número de alunos: n - int
+# conjunto de alunos: estudante - dicionario
+# nome dos alunos: nome - string
+# sobrenome do aluno: snome - string
+# notas dos alunos: notas - float
 
-# pré: para i em {0..tam-1}: numero, nome, notas[0] notas[1] notas[2]
-#      notas[3] notas[4]
+# Funções
+# ordenar os alunos em ordem alfabética pelo primeiro nome: ordena(dados - lista)
+def ordenaInsert(lista):
+    print(lista)
+    # Leia o tamanho da lista
+    tam = len(lista)
+    # Ordenação
+    for i in range(1, tam):
+        temp = lista[i]
+        del lista[i]
+        # Percorra a lista pelo final
+        j = i - 1
+        # Enquanto o percurso for menor ou igual a 0 e a temporária for menor que o
+        # elemento j da lista, faça:
+        while j <= 0 and temp < lista[j]:
+            # Retorne com o índice pela esquerda.
+            j -= 1
+        # Insira a temporária na posição de j
+        lista.insert(j + 1, temp)
 
-# Passo 1. Leia o entrada e inicialize as variáveis
-# Passo 1.1. Leia o número de alunos
-tam = int(input())
+    return lista
 
-# Passo 1.2. Inicialize as variáveis
-chaves = ['numero', 'nome', 'notas']  # chaves do dicionários
+
+# calcula a média: media()
+
+def media(lista):
+
+    # Média dos Trabalhos
+    mt = sum(lista[3::]) / 2
+    # Média das Provas
+    mp = sum(lista[0:3]) / 3
+    # Média Final
+    valor = 0.75 * mp + 0.25 * mt
+
+    return valor
+
+
+# Passo 1. Leia a entrada
+# Passo 1.1. Leia o tamanho do conjunto de conjunto de teste
+n = int(input())
+# Passo 1.2. Leia os dados dos estudantes
+chaves = ['nome', 'snome', 'notas', 'mf']
 estudante = dict.fromkeys(chaves)
-dados = []  # lista de strings para ler a entrada
+dados = []
+# Passo 2. Ordene os dados
+for i in range(n):
+    dados = input().split()
+    estudante['nome'] = dados[0]
+    estudante['snome'] = dados[1]
+    estudante['notas'] = list(map(float, dados[2].split()))
 
-# Passo 1.3. Leia os dados dos alunos
-for i in range(tam):
-    dados = input().split(', ')  # lido como lista de strings
-    estudante[i]['numero'] = int(dados[0])  # converta para inteiros
-    estudante[i]['nome'] = dados[1]
-    estudante[i]['notas'] = list(map(float, dados[2].split()))  # lista floats
 
-    # Passo 1.3.2. Calcule a média final do aluno
-    # Passo 1.3.2.1. Calcule a média das provas
-    mediaprovas = sum(estudante['notas'][:3]) / 3.0
+    # Calcule a média
+    # Média dos Trabalhos
+    mt = sum(estudante['notas'][3::]) / 2
+    # Média das Provas
+    mp = sum(estudante['notas'][0:3]) / 3
+    # Média Final
+    mf = 0.75 * mp + 0.25 * mt
 
-    # Passo 1.3.2.2. Calcule a média dos trabalhos
-    mediatrab = sum(estudante['notas'][3:]) / 2.0
+    #estudante['mf'] = mf
 
-    # Passo 1.3.2.3. Calcule a média final
-    mediafinal = 0.75 * mediaprovas + 0.25 * mediatrab
+print(estudante['nome'])
 
-    # Passo 1.3.2.4. Imprime os resultados
-    print('{0:s} {1:s} {2:.1f}'.format(estudante[i]['nome'], estudante[i]['snome'],
-                                       estudante[i]['mf']))
+# Ordene a lista pelo nome
+#for i in range(1, n):
+    #temp = estudante[i]
 
-# pós: para i em {0..tam-1}: nome, mediafinal[i] and
-#      mediafinal[i] == 0.75*mediaprovas[i]+0.25*mediatrabalhos[i]
-#      and mediaprovas[i] ==(notas[0] + notas[1] + notas[2])/3.0
-#      and mediatrabalhos[i] == (notas[3] + notas[4])/2.0
+# Imprima o resultado
+#for i in range(n):
+    #print('{0:s} {1:s} {2:.1f}'.format(estudante[i]['nome'], estudante[i]['snome'],
+                               #estudante[i]['mf']))
+
 # fim do módulo principal
